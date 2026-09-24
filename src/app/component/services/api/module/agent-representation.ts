06175db86590feb5848df34d8402b6e8cc940169
Vinura Dayal <vinuradayalw@gmail.com>
2026-09-25 00:06:33 +0530

    Fixed and removed student fields in agent comp

--- src/app/component/services/api/module/agent-representation.ts
+++ src/app/component/services/api/module/agent-representation.ts
@@ -6,9 +6,9 @@ export interface StatusRepresentation {
 export interface AgentRepresentation {
   id?: string | number;
   agentCode?: string;
-  agentName?: string;
-  agentAge?: string;
-  agentNic?: string;
+  licenseNo?: string;
+  commissionRate?: string;
+  phone?: string;
   /** Request: status id. Response: { id, name } */
   status?: any;
 }
